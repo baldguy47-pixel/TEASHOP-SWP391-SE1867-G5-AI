a3528d03641de282fe26011eaa306dfbbfd1493f
linhtkhe171737 <linhtkhe171737@fpt.edu.vn>
2024-10-15 23:17:15 +0700

    productDAO

--- TeaOrderSystem/src/DAO/ProductDAO.java
+++ TeaOrderSystem/src/DAO/ProductDAO.java
@@ -153,3 +153,131 @@ public class ProductDAO extends DBContext {
 
         return products;
     }
+    public List<Product> getAllProducts() {
+        List<Product> products = new ArrayList<>();
+
+        String sql = "SELECT "
+                + "p.ID AS ProductID, "
+                + "p.Name AS ProductName, "
+                + "c.Name AS CategoryName, "
+                + "pd.ID AS ProductDetailID, "
+                + "pd.ImageURL, "
+                + "pd.Size, "
+                + " "
+                + "pd.Stock, "
+                + "pd.price AS price, "
+                + "pd.discount AS discount, "
+                + "pd.CreatedAt AS ProductDetailCreatedAt, "
+                + "pd.CreatedBy AS ProductDetailCreatedBy "
+                + "FROM Product p "
+                + "INNER JOIN Category c ON p.CategoryID = c.ID "
+                + "INNER JOIN ProductDetail pd ON p.ID = pd.ProductID "
+                + "WHERE p.IsDeleted = 0 AND pd.IsDeleted = 0 "
+                + "ORDER BY p.ID ASC";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql); ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                Product product = new Product();
+                product.setProductId(resultSet.getInt("ProductID"));
+                product.setProductName(resultSet.getString("ProductName"));
+                product.setCategoryName(resultSet.getString("CategoryName"));
+
+                ProductDetail productDetail = new ProductDetail();
+                productDetail.setProductDetailId(resultSet.getInt("ProductDetailID"));
+                productDetail.setImageURL(resultSet.getString("ImageURL"));
+                productDetail.setSize(resultSet.getString("Size"));
+
+                productDetail.setStock(resultSet.getInt("Stock"));
+                productDetail.setCreatedAt(resultSet.getTimestamp("ProductDetailCreatedAt"));
+                productDetail.setCreatedBy(resultSet.getInt("ProductDetailCreatedBy"));
+                productDetail.setPrice(resultSet.getDouble("price"));
+                productDetail.setDiscount(resultSet.getInt("discount"));
+
+                product.setProductDetail(productDetail);
+
+                products.add(product);
+            }
+        } catch (SQLException ex) {
+            System.out.println("getAllProducts: " + ex.getMessage());
+        }
+
+        return products;
+    }
+
+    public List<Product> getProductsByPage(int pageNumber, int pageSize) {
+        List<Product> products = new ArrayList<>();
+        int offset = (pageNumber - 1) * pageSize;
+
+        String sql = "SELECT distinct\n"
+                + "p.ID AS ProductID,\n"
+                + "p.Name AS ProductName,\n"
+                + "c.Name AS CategoryName\n"
+                + "FROM Product p\n"
+                + "INNER JOIN Category c ON p.CategoryID = c.ID\n"
+                + "WHERE p.IsDeleted = 0\n"
+                + "ORDER BY p.ID ASC\n"
+                + "LIMIT ? OFFSET ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(2, offset);
+            statement.setInt(1, pageSize);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                Product product = new Product();
+                product.setProductId(resultSet.getInt("ProductID"));
+                product.setProductName(resultSet.getString("ProductName"));
+                product.setCategoryName(resultSet.getString("CategoryName"));
+
+                ProductDetail productDetail = getProductDetailByProductId(product.getProductId());
+                product.setProductDetail(productDetail);
+                products.add(product);
+            }
+        } catch (SQLException ex) {
+            System.out.println("getProductsByPage: " + ex.getMessage());
+        }
+
+        return products;
+    }
+
+    public ProductDetail getProductDetailByProductId(int productId) {
+
+        String sql = "SELECT "
+                + "pd.ID AS ProductDetailID, "
+                + "pd.ImageURL, "
+                + "pd.Size, "
+                + "pd.Stock, "
+                + "pd.price, "
+                + "pd.discount, "
+                + "pd.CreatedAt, "
+                + "pd.Hold, "
+                + "pd.ImportPrice, "
+                + "pd.CreatedBy "
+                + "FROM ProductDetail pd "
+                + "WHERE pd.ProductID = ? AND pd.IsDeleted = 0";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, productId);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                ProductDetail productDetail = new ProductDetail();
+                productDetail.setProductDetailId(resultSet.getInt("ProductDetailID"));
+                productDetail.setImageURL(resultSet.getString("ImageURL"));
+                productDetail.setSize(resultSet.getString("Size"));
+                productDetail.setStock(resultSet.getInt("Stock"));
+                productDetail.setPrice(resultSet.getDouble("price"));
+                productDetail.setDiscount(resultSet.getInt("discount"));
+                productDetail.setCreatedAt(resultSet.getTimestamp("CreatedAt"));
+                productDetail.setCreatedBy(resultSet.getInt("CreatedBy"));
+                productDetail.setHold(resultSet.getInt("Hold"));
+                productDetail.setImportPrice(resultSet.getFloat("ImportPrice"));
+                return productDetail;
+            }
+        } catch (SQLException ex) {
+            System.out.println("getProductDetailsByProductId: " + ex.getMessage());
+        }
+
+        return new ProductDetail();
+    }
